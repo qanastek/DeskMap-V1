211272a1d93b2333538e7e9c48bf3fd70925d23d
Vincent Labatut <vince.labatut@gmail.com>
2019-09-23 09:22:22 +0200

    main package and class

---
+++ src/fr/univavignon/ceri/deskmap/Launcher.java
@@ -0,0 +1,22 @@
+package fr.univavignon.ceri.deskmap;
+
+/**
+ * This class is used to launch the game.
+ * 
+ * @author Author 1
+ * @author Author 2
+ * @author Author 3
+ * @author Author 4
+ */
+public class Launcher
+{	
+	/**
+	 * Launches the game.
+	 * 
+	 * @param args
+	 * 		Not used here.
+	 */
+	public static void main(String[] args)
+	{	//TODO to be completed
+	}
+}
