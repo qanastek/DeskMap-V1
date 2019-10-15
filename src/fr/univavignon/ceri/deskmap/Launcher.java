59cad2ce1c0da8c934b4bcd3ffb6b893bfc611e0
Mohamed BEN YAMNA <benyamna@10.126.3.16>
2019-10-15 13:03:23 +0200

    -The two buttons RESET and RESEARCH added. -Added about us dialog

--- src/fr/univavignon/ceri/deskmap/Launcher.java
+++ src/fr/univavignon/ceri/deskmap/Launcher.java
@@ -1,5 +1,26 @@
 package fr.univavignon.ceri.deskmap;
 
+import java.awt.geom.Point2D;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
 /**
  * This class is used to launch the software.
  * 
@@ -8,7 +29,7 @@ package fr.univavignon.ceri.deskmap;
  * @author Yanis Labrak
  * @author Zihao Zheng
  */
-public class Launcher
+public class Launcher extends Application
 {	
 	/**
 	 * Launches the software.
@@ -18,6 +39,34 @@ public class Launcher
 	 * 		Not used here.
 	 */
 	public static void main(String[] args)
-	{	//TODO to be completed
+	{
+		launch(args);
 	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// Set the title of the window
+		primaryStage.setTitle("DESKMAP V1.0.18");
+
+		// Get the dimensions of the monitor
+	    Rectangle2D screenBounds = Screen.getPrimary().getBounds();
+		
+	    // Create and link the layout to a FXML file
+	    // This is the 'WINDOW' of the app
+	    Parent layout = FXMLLoader.load(getClass().getResource("MainView.fxml"));
+
+	    // The area inside of the layout
+		Scene my_scene = new Scene(layout, layout.getLayoutY(), layout.getLayoutX());
+		
+		// Set the width of the window to the half of the monitor width
+		primaryStage.setMinWidth(screenBounds.getWidth() * 0.5);
+
+		// Set the height of the window to the half of the monitor height
+		primaryStage.setMinHeight(screenBounds.getHeight() * 0.5);
+		
+		primaryStage.setScene(my_scene);
+		primaryStage.show();
+	}
+	
+	
 }
