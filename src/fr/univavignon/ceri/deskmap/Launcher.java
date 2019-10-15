41870fe84b8ae7f6a62aeba8729c78677d391c45
ZIHAO ZHENG <zihaoz96@gmail.com>
2019-10-15 13:05:20 +0200

    - I create few input fields and buttons on the interface - I also create the controllers for limiting the inputs of the street number field to only accept digits

--- src/fr/univavignon/ceri/deskmap/Launcher.java
+++ src/fr/univavignon/ceri/deskmap/Launcher.java
@@ -2,22 +2,46 @@ package fr.univavignon.ceri.deskmap;
 
 /**
  * This class is used to launch the software.
- * 
+ *
  * @author Mohamed BEN YAMNA
  * @author Quentin Capdepon
  * @author Yanis Labrak
  * @author Zihao Zheng
  */
-public class Launcher
-{	
-	/**
-	 * Launches the software.
-	 * Add things for testing
-	 * 
-	 * @param args
-	 * 		Not used here.
-	 */
-	public static void main(String[] args)
-	{	//TODO to be completed
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author zihao
+ *
+ */
+public class Launcher extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+
+		primaryStage.setMinHeight(650);
+
+		primaryStage.setMinWidth(1280);
+
+		primaryStage.setTitle("PUBG");
+
+
+
+		Parent layout = FXMLLoader.load(getClass().getResource("test.fxml"));
+
+		Scene my_scene = new Scene(layout, layout.getLayoutY(), layout.getLayoutX());
+
+		primaryStage.setScene(my_scene);
+
+
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
 	}
 }
---
+++ src/fr/univavignon/ceri/deskmap/testController.java
@@ -0,0 +1,117 @@
+package fr.univavignon.ceri.deskmap;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.stream.Stream;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.SplitMenuButton;
+
+
+
+public class testController implements Initializable {
+	@FXML TextField nStreet;
+	@FXML TextField nomVille;
+	@FXML ComboBox<String> cbb;
+	@FXML ComboBox<String> cbbTmp;
+	@FXML SplitMenuButton nomStreet;
+	@FXML Button btn;
+	@FXML AnchorPane anchorPane1,anchorPane2;
+	ObservableList<String> originalItems;
+
+    @Override
+	public void initialize(URL location, ResourceBundle resources) {
+    	anchorPane1.setMinWidth(345);
+		btn.setStyle("-fx-background-color: #B2BABB;");
+		cbb.getItems().setAll("Apple", "Orange", "Pear","Abc"
+				,"Abd","Abe","Abf pp ff","Abg","Abh","Abi","Bbc",
+				"Bbd","Bbe","Bbf","Bbg","Bbh","Cb1","Cb2","Cb3"
+				,"Cb4","Cb5","Cb6","D1","D2","D3");
+
+		this.cbb.setEditable(true);
+		this.originalItems= FXCollections.observableArrayList(this.cbb.getItems());
+//		new ComboBoxAutoComplete<String>(cbb);
+
+	}
+
+
+	@FXML
+	private void TextType(KeyEvent event){
+		if(!Character.isDigit(event.getCharacter().charAt(0))){
+			event.consume();
+		}
+	}
+
+	@FXML
+	private void checkVille(KeyEvent event){
+		if (nomVille.getText().equals("")){
+			btn.setStyle("-fx-background-color: #d0d0d0;");
+		}
+		else {
+			this.btn.setStyle("-fx-background: #f4f4f4;");
+		}
+	}
+	/**
+	 * @param event e
+	 */
+	@FXML
+	private void okClick(MouseEvent event)
+    {
+		if(!this.nomVille.getText().equals("")){
+			this.btn.setStyle("-fx-color: #82E0AA;");
+		}
+    }
+	/**
+	 * @param event e
+	 */
+	@FXML
+	private void comboboxToStreet(KeyEvent event){
+
+		ObservableList<String> filteredList = FXCollections.observableArrayList();
+
+		String filter = cbb.getEditor().getText();
+
+
+		if (event.getCode().equals(KeyCode.BACK_SPACE) && filter.length() > 0) {
+			filter = filter.substring(0, filter.length() - 1);
+			this.cbb.getItems().setAll(originalItems);
+		}
+		System.out.println(filter);
+		if(filter.length() != 0){
+			Stream<String> itens = cbb.getItems().stream();
+			String txtUsr = filter.toString().toLowerCase();
+			itens.filter(el -> el.toString().toLowerCase().contains(txtUsr)).forEach(filteredList::add);
+
+			cbb.show();
+		}
+
+		cbb.getItems().setAll(filteredList);
+	}
+
+	@FXML
+	private void Searching(MouseEvent event)
+    {
+		System.out.println("Searching clicked");
+
+    }
+
+	@FXML
+	private void SetCity(MouseEvent event)
+    {
+		System.out.println(nStreet.getText());
+
+    }
+
+
+
+}
