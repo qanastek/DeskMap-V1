68e60929c754ae46f9eae06426ed93f1c5d720be
qanastek <yanis.labrak@alumni.univ-avignon.fr>
2019-10-29 23:37:22 +0100

    - Testing before merge the projection

--- src/fr/univavignon/ceri/deskmap/Node.java
+++ src/fr/univavignon/ceri/deskmap/Node.java
@@ -1,5 +1,8 @@
 package fr.univavignon.ceri.deskmap;
 
+import java.awt.Point;
+import java.awt.geom.Point2D;
+
 /**
  * A class which represent the structure of an OSM Node
  * @author Yanis Labrak
@@ -44,5 +47,54 @@ public class Node {
 		this.id = id;
 		this.lat = lat;
 		this.lon = lon;
+		this.Node_projection();
+	}
+	
+	@Override
+	public String toString() {
+		return "[Id: " + this.id + ", Lat: " + this.lat + ", Lon: " + this.lon + "]";
+	}
+	
+	/**
+	 * Hyperbolic Tangent of
+	 * @param x {@code Double} Value
+	 * @return {@code Double} Return the hyperbolic Tangent of value
+	 */
+	static double atanh(double x)
+	{
+		return ( 0.5*Math.log((x + 1.0) / (1.0 - x) ));
+	}
+	
+	
+	/**
+	 * Convert from WGS-84 format to RGF93
+	 * @author loic.buscoz <i>Author</i>
+	 * @author BEN-YAMNA Mohamed <i>Adaptation</i>
+	 * @see <a href="https://georezo.net/forum/viewtopic.php?id=94465">Conversion WGS84 vers RGF93 en PHP</a>
+	 */
+	public void Node_projection() {
+		
+	    double c = 11754255.426096;
+	    double e = 0.0818191910428158;
+	    double n = 0.725607765053267;
+	    double xs = 700000;
+	    double ys = 12655612.049876;
+		
+	    double lat_rad = this.lat/180*3.14159265359;
+	    double lat_iso = Node.atanh(Math.sin(lat_rad))-e*Node.atanh(e*Math.sin(lat_rad));
+
+	    double x = ((c*Math.exp(-n*(lat_iso)))*Math.sin(n*(this.lon-3)/180*3.14159265359)+xs);
+	    double y = (ys-(c*Math.exp(-n*(lat_iso)))*Math.cos(n*(this.lon-3)/180*3.14159265359));
+	    
+	    System.out.println(x);
+	    System.out.println(y);
+	    
+	    this.lat = x;
+	    this.lon = y;
+	}
+	
+	public static void main(String[] args) {
+		Node n = new Node("484848484", 48.8566969, 2.3514616);
+		System.out.println(n.toString());
 	}
 }
